6a7e669d2ea54743d64c79cfe3b63648ca13269c
Paul Hamilton <phamtec@mac.com>
2013-07-01 17:23:26 +1000

    Lock down integration test config

--- src/integration-test/test.sh
+++ src/integration-test/test.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
 rm *.out
-monitor/bin/darwin-4.2.1/debug/threading-multi/monitor machine.json > monitor.out
+monitor/bin/darwin-4.2.1/debug/threading-multi/monitor integration-test/test.json > monitor.out
 sleep 3
+
+# sink finds everything
 cat sink.out | grep "Total elapsed time: " > /dev/null
-if [ "$?" == "0" ]
+if [ "$?" != "0" ]
 then
-	echo "PASSED"
-else
 	echo "FAILED"
 fi
+
+# at least a little work for each task.
+cat task0.out | grep "...." > /dev/null
+if [ "$?" != "0" ]
+then
+	echo "FAILED"
+fi
+cat task1.out | grep "...." > /dev/null
+if [ "$?" != "0" ]
+then
+	echo "FAILED"
+fi
+cat task2.out | grep "...." > /dev/null
+if [ "$?" != "0" ]
+then
+	echo "FAILED"
+fi
+
+
+echo "PASSED"
